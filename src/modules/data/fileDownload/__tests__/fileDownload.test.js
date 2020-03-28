215608a13e122d946cf6b2a60a324559121d3073
David Walther <walthdav@gmail.com>
2020-03-28 23:13:41 +0100

    create test syntax

---
+++ src/modules/data/fileDownload/__tests__/fileDownload.test.js
@@ -0,0 +1,10 @@
+describe('Download', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  test('', () => {});
+});
