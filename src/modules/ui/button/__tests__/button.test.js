6d11d9c95b415b89fa2bc993a0bd7ea300e19e3a
David Walther <walthdav@gmail.com>
2020-03-29 15:49:21 +0200

    add new test for checking event

--- src/modules/ui/button/__tests__/button.test.js
+++ src/modules/ui/button/__tests__/button.test.js
@@ -52,13 +52,42 @@ describe('Layout', () => {
     expect(button.classList.contains('button')).toBe(true);
     expect(button.classList.contains('info')).toBe(true);
   });
+});
 
-  describe('Behavior', () => {
-    afterEach(() => {
-      // The jsdom instance is shared across test cases in a single file so reset the DOM
-      while (document.body.firstChild) {
-        document.body.removeChild(document.body.firstChild);
-      }
+describe('Behavior', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  test('Event is fired when button is clicked', () => {
+    const handler = jest.fn();
+
+    /**
+     * Given
+     * The component is inserted in the DOM
+     */
+    const element = createElement('ui-button', { is: Button });
+    element.addEventListener('click', handler);
+    document.body.appendChild(element);
+
+    /**
+     * When
+     * The the button is clicked
+     */
+    const button = element.shadowRoot.querySelector('input');
+    expect(button).toBeDefined();
+    button.dispatchEvent(new CustomEvent('click'));
+
+    //wait for click event to be processed
+    return Promise.resolve().then(() => {
+      /**
+       * Then
+       * The click event can be catched by the parent
+       */
+      expect(handler).toHaveBeenCalled();
     });
   });
 });
