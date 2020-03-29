9e99e5d37bc015ef695cb068b05615d19329fa1c
David Walther <walthdav@gmail.com>
2020-03-29 12:40:03 +0200

    create test setup

---
+++ src/modules/ui/button/__tests__/button.test.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { createElement } from 'lwc';
+// eslint-disable-next-line no-unused-vars
+import ModalButton from 'ui/entry';
+
+describe('Layout', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  test('', () => {});
+});
