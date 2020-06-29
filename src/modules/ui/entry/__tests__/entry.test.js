0b026a5090a24701b3f64730b24b18e95f274624
David Walther <walthdav@gmail.com>
2020-06-29 10:59:36 +0200

    create comment suite

--- src/modules/ui/entry/__tests__/entry.test.js
+++ src/modules/ui/entry/__tests__/entry.test.js
@@ -587,6 +587,17 @@ describe('feature - break time', () => {
   });
 });
 
+describe('feature - comment', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  
+});
+
 describe('check single entry delete', () => {
   afterEach(() => {
     while (document.body.firstChild) {
