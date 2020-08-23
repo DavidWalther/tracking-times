e6021126e2b3ba4d2ae07b14e76877944c14cc4e
David Walther <walthdav@gmail.com>
2020-08-23 12:39:25 +0200

    create date input

--- src/modules/ui/inputDateTime/__tests__/inputDateTime.test.js
+++ src/modules/ui/inputDateTime/__tests__/inputDateTime.test.js
@@ -28,3 +28,31 @@ describe('Time Input', () => {
     expect(inputTime).toBeTruthy();
   });
 });
+
+describe('Date input', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild);
+    }
+  });
+
+  test('component has an input for date', () => {
+    /**
+     * Given
+     * -
+     */
+    /**
+     * When
+     * the component is added to DOM
+     */
+    const element = createElement('ui-input-date-time', { is: InputDateTime });
+    document.body.appendChild(element);
+    /**
+     * Then
+     * It has an input for time values
+     */
+    const inputTime = element.shadowRoot.querySelector('.input-date');
+    expect(inputTime).toBeTruthy();
+  });
+});
