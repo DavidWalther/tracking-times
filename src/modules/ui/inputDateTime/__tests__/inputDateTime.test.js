2759abbcdde4b01bc28d47732b4c79e6a75782b6
David Walther <walthdav@gmail.com>
2020-08-23 12:28:28 +0200

    create time input

--- src/modules/ui/inputDateTime/__tests__/inputDateTime.test.js
+++ src/modules/ui/inputDateTime/__tests__/inputDateTime.test.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-unused-vars
 import { createElement } from 'lwc';
-// eslint-disable-next-line no-unused-vars
 import InputDateTime from 'ui/inputDateTime';
 
 describe('Time Input', () => {
@@ -20,9 +18,13 @@ describe('Time Input', () => {
      * When
      * the component is added to DOM
      */
+    const element = createElement('ui-input-date-time', { is: InputDateTime });
+    document.body.appendChild(element);
     /**
      * Then
      * It has an input for time values
      */
+    const inputTime = element.shadowRoot.querySelector('.input-time');
+    expect(inputTime).toBeTruthy();
   });
 });
