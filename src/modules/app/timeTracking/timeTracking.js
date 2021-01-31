a9d9b85597503651cc04122dbab10380127d59c3
David Walther <walthdav@gmail.com>
2021-01-31 12:35:39 +0100

    create click handler

--- src/modules/app/timeTracking/timeTracking.js
+++ src/modules/app/timeTracking/timeTracking.js
@@ -118,6 +118,8 @@ export default class TimeTracking extends LightningElement {
     this.selectAllEntries();
   }
 
+  handleClickSort() {}
+
   //----------------------------
   // Properties
   //----------------------------
