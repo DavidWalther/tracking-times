1e080c8fe0060a0cd4157e76f5e08f30b647cad4
David Walther <walthdav@gmail.com>
2021-03-20 21:53:15 +0100

    create basic attributes for connection

--- src/modules/data/salesforceInterface/salesforceInterface.js
+++ src/modules/data/salesforceInterface/salesforceInterface.js
@@ -1,5 +1,9 @@
-import {LightningElement} from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class SalesforceInterface extends LightningElement {
-  
+  @api
+  accessToken;
+
+  @api
+  instanceUrl;
 }
