172aa957cc2f039031f51c0780d986d66bc4697f
David Walther <walthdav@gmail.com>
2021-03-21 00:29:03 +0100

    create a logic for create record callout

--- src/modules/data/salesforceInterface/salesforceInterface.js
+++ src/modules/data/salesforceInterface/salesforceInterface.js
@@ -13,17 +13,32 @@ export default class SalesforceInterface extends LightningElement {
   instanceUrl;
 
   @api
-  createRecord(recordJsonObj) {
-    this.salesforceCreateRecord(recordJsonObj);
+  createRecord(objectType, recordJsonObj) {
+    if (this.accessToken && this.instanceUrl && objectType) {
+      this.salesforceCreateRecord(objectType, recordJsonObj);
+    }
   }
 
   //----------------------------
   // internal
   //----------------------------
 
-  salesforceCreateRecord(recordJsonObj) {
+  salesforceCreateRecord(objectType, recordJsonObj) {
+    /*
     console.log('instanceUrl: ' + this.instanceUrl);
     console.log('token: ' + this.accessToken);
     console.log('recordJsonObj: ' + recordJsonObj);
+*/
+    const salesforceApiVersion = 'v51.0';
+    const createRecordEndpoint =
+      '/services/data/' + salesforceApiVersion + '/sobjects/';
+
+    const theUrl = this.instanceUrl + createRecordEndpoint + objectType + '/';
+
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', theUrl);
+    xmlhttp.setRequestHeader('Content-Type', 'application/json');
+    xmlhttp.setRequestHeader('Authorization', 'Bearer ' + this.access_token);
+    xmlhttp.send(JSON.stringify(recordJsonObj));
   }
 }
