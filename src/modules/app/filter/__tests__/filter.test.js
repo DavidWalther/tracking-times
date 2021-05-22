a3921ed00de5db070e85dfafaf39679c0782fe1c
David Walther <walthdav@gmail.com>
2021-05-22 14:39:46 +0200

    create filter sections for text and date

--- src/modules/app/filter/__tests__/filter.test.js
+++ src/modules/app/filter/__tests__/filter.test.js
@@ -1,9 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 import { createElement } from 'lwc';
-// eslint-disable-next-line no-unused-vars
 import Filter from 'app/filter';
 
-describe('', () => {
+describe('attributes', () => {
   afterEach(() => {
     // The jsdom instance is shared across test cases in a single file so reset the DOM
     while (document.body.firstChild) {
@@ -11,5 +9,25 @@ describe('', () => {
     }
   });
 
-  test('', () => {});
+  describe('filter-type', () => {
+    test("text filter section is displayed on type 'text'.", () => {
+      const element = createElement('app-filter', { is: Filter });
+      element.filterType = 'text';
+      document.body.appendChild(element);
+
+      const elements_ToCheck = element.shadowRoot.querySelectorAll('.filter');
+      expect(elements_ToCheck.length).toBe(1);
+      expect(elements_ToCheck[0].classList).toContain('filter-text');
+    });
+
+    test("date filter section is displayed on type 'date'.", () => {
+      const element = createElement('app-filter', { is: Filter });
+      element.filterType = 'date';
+      document.body.appendChild(element);
+
+      const elements_ToCheck = element.shadowRoot.querySelectorAll('.filter');
+      expect(elements_ToCheck.length).toBe(1);
+      expect(elements_ToCheck[0].classList).toContain('filter-date');
+    });
+  });
 });
--- src/modules/app/filter/filter.js
+++ src/modules/app/filter/filter.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+/**
+ * This component contains a single filter setting for a certain type of data.
+ */
+import { LightningElement, api } from 'lwc';
 
-export default class Filter extends LightningElement {}
+export default class Filter extends LightningElement {
+  //----------------------------
+  // API
+  //----------------------------
+
+  /**
+   * The available operators are defined by the provided filter type.
+   */
+  @api
+  filterType;
+
+  get isFilterTypeText() {
+    return this.filterType === 'text';
+  }
+
+  get isFilterTypeDate() {
+    return this.filterType === 'date';
+  }
+}
